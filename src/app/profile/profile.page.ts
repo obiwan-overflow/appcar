19e3115ce07be0e7a2df9d6124d51f9d6200ca2c
obiwan-overflow <kittipopp@hotmail.co.th>
2021-04-20 17:22:19 +0700

    update app

--- src/app/profile/profile.page.ts
+++ src/app/profile/profile.page.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { Storage } from '@ionic/storage';
 import { ActivatedRoute, Router } from '@angular/router';
 import { AlertController, LoadingController } from '@ionic/angular';
+import { RestApiService } from '../rest-api.service';
 
 @Component({
   selector: 'app-profile',
@@ -10,16 +11,40 @@ import { AlertController, LoadingController } from '@ionic/angular';
 })
 export class ProfilePage implements OnInit {
   name:any;
-  constructor(private storage: Storage,public route: Router,public loadingController: LoadingController) {
-  	this.storage.get('token').then((data) => {
-    	if(data == ''){
-    		this.route.navigate(['/login']);
-    	}
-    	this.storage.get('name').then((data) => {
-      	this.name = data
-      });
-    });
+  token:any;
+  login:any;
+  profile:any;
+  fullname:any;
+  coin:any;
+  image:any;
+  constructor(private storage: Storage,public route: Router,public loadingController: LoadingController,public api:RestApiService) {
+  	// this.storage.get('token').then((data) => {
+   //  	if(data == ''){
+   //  		this.route.navigate(['/login']);
+   //  	}
+   //  	this.storage.get('name').then((data) => {
+   //    	this.name = data
+   //    });
+   //  });
     
+  }
+  async ionViewWillEnter(){
+    await this.storage.get('token').then((data)=>{
+      if (data == null) {
+        this.login = "failed";
+      }else{
+        this.login = "success";
+        this.token = data;
+      }
+    })
+    this.api.getdata('profile/getProfile&token='+this.token).subscribe(res=>{
+      this.profile     = res;
+      this.fullname    = this.profile.Name+" "+this.profile.Surname;
+      this.coin        = this.profile.coin;
+      this.image       = this.profile.path_image_resize;
+    },err=>{
+      console.log(err);
+    })
   }
 
   ngOnInit() {
